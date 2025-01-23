ebd2205a1e25ac19f7601a0de6d2e5cc0b010270
Brendan Moorehead <brendan.moorehead11@gmail.com>
2025-01-23 00:11:06 -0500

    basic script for polling API for NFL teams

--- backend/polling-script/pollNFL.js
+++ backend/polling-script/pollNFL.js
@@ -1,3 +1,21 @@
-// Main NFL API polling script
-require('dotenv').config();
-const cron = require('node-cron');
+import dotenv from 'dotenv';
+import axios from 'axios';
+
+dotenv.config();
+
+async function fetchNFLTeams() {
+  try {
+    console.log('Fetching NFL teams...');
+    if (!process.env.NFL_API_KEY)
+      throw new Error('NFL API key is missing. Check .env file.');
+    const response = await axios.get(
+      `https://api.sportsdata.io/v3/nfl/scores/json/TeamsBasic?key=${process.env.NFL_API_KEY}`
+    );
+    console.log('NFL Teams fetched.');
+    //TODO: Add NFL Teams to database
+    console.log('NFL Teams:', response.data);
+  } catch (error) {
+    console.error('Error in fetchNFLTeams: ', error.message);
+  }
+}
+fetchNFLTeams();
