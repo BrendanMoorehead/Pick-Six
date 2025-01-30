33db313429bca844ec201ab1bb46dc7ee7909fd0
Brendan Moorehead <brendan.moorehead11@gmail.com>
2025-01-29 22:40:38 -0500

    check for taken username and email

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,7 +4,23 @@ export async function registerUser(req, res) {
   if (!email || !password || !username)
     return res.status(400).json({ error: 'All fields required' });
   try {
+    const { data: existingUser, error: fetchError } = await supabase
+      .from('users')
+      .select('email')
+      .eq('email', email)
+      .single();
+    if (existingUser)
+      return res.status(400).json({ error: 'Email is already registered' });
+    const { data: existingUsername, error: usernameFetchError } = await supabase
+      .from('users')
+      .select('username')
+      .eq('username', username)
+      .single();
+    if (existingUsername)
+      return res.status(400).json({ error: 'Username is already registered' });
+
     const { data, error } = await supabase.auth.signUp({ email, password });
+    console.log(data);
     if (error) return res.status(400).json({ error: error.message });
     await supabase
       .from('users')
