9aed18c9cb0e33b3231c70db73d6777f0119bfb7
Brendan Moorehead <brendan.moorehead11@gmail.com>
2025-01-29 19:27:54 -0500

    server starts

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-
-const authRoutes = require('./routes/auth');
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+// import authRoutes from './routes/auth.js';
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/auth', authRoutes);
+// app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
