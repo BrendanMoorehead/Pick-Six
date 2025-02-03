835e8640cb6c6caa4ffdf59a3847623414e03f68
Brendan Moorehead <brendan.moorehead11@gmail.com>
2025-02-03 17:05:55 -0500

    auth context for supabase

---
+++ frontend/src/contexts/AuthContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import { supabase } from '../../supabaseClient';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkUser = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getUser();
+      setUser(session?.user || null);
+      setLoading(false);
+    };
+    checkUser();
+
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        setUser(session?.user || null);
+        setLoading(false);
+      }
+    );
+
+    return () => {
+      listener.subscription.unsubscribe();
+    };
+  }, []);
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
