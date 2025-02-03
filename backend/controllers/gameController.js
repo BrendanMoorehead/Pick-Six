9da2a4d6381aff6b5a83faa3743e979c9996988f
Brendan Moorehead <brendan.moorehead11@gmail.com>
2025-02-03 15:39:32 -0500

    get games for a given season endpoint

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -2,9 +2,17 @@
 import supabase from '../db.js';
 
 export async function getGames(req, res) {
+  const season = req.query.season;
+  if (!season) return res.status(400).json({ error: 'Missing parameters' });
   try {
-    const { data, error } = await supabase.from('nfl_games').select();
-    console.log(data);
+    const { data, error } = await supabase.rpc('get_nfl_games_with_teams', {
+      season_param: season,
+    });
+    if (error)
+      return res
+        .status(400)
+        .json({ error: `Failed to get nfl games for ${season} season` });
+    res.status(200).json(data);
   } catch (error) {
     console.error('Server Error', error);
     res.status(500).json({ error: 'Server error' });
