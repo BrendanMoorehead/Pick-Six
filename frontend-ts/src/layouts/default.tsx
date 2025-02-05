c29af0ab02453b307953780234d24408a7f8bb37
Brendan Moorehead <brendan.moorehead11@gmail.com>
2025-02-04 19:40:47 -0500

    heroui frontend setup

---
+++ frontend-ts/src/layouts/default.tsx
@@ -0,0 +1,11 @@
+import { Link } from '@heroui/link';
+
+import { Navbar } from '@/components/navbar';
+
+export default function DefaultLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div className="relative flex flex-col h-screen">{children}</div>;
+}
---
+++ frontend-ts/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import LoginForm from '@/components/forms/LoginForm';
+import DefaultLayout from '@/layouts/default';
+import { Input } from '@heroui/input';
+
+export default function IndexPage() {
+  return (
+    <DefaultLayout>
+      <p>HELLO BAND</p>
+      <Input />
+      <LoginForm />
+    </DefaultLayout>
+  );
+}
