61493c18dbb0cf76b9ff792f07fecb61c41f8447
Brendan Moorehead <brendan.moorehead11@gmail.com>
2025-02-18 17:45:24 -0500

    pick week wrapper and start game slice

---
+++ frontend-ts/src/components/picks/PickWeekWrapper.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const PickWeekWrapper = () => {
+  //Games are passed to the PickWeekWrapper and the GameWrapper is populated from here.
+  //User picks need to be fetched to populate too
+  //Picks are uploaded on change from here, saved icon should be shown, potentially run a timer to group populate, timer resets on click
+  return <div>PickWeekWrapper</div>;
+};
+
+export default PickWeekWrapper;
