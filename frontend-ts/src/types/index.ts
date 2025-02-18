ad2cdf477323f6f48cacf89de4dfdfa1389ab10d
Brendan Moorehead <brendan.moorehead11@gmail.com>
2025-02-18 17:47:31 -0500

    build out game type to match database

--- frontend-ts/src/types/index.ts
+++ frontend-ts/src/types/index.ts
@@ -15,3 +15,15 @@ export type Team = {
   secondary_color: string;
   tertiary_color?: string;
 };
+
+export type Game = {
+  id: number;
+  game_key: string;
+  season_type: number;
+  week: number;
+  away_team_id: number;
+  home_team_id: number;
+  score_id: number;
+  status: string;
+  game_id: number;
+};
