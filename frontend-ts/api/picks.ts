23ad79da6e25aab7a76ad2e0b565577e8e942711
Brendan Moorehead <brendan.moorehead11@gmail.com>
2025-02-19 13:02:57 -0500

    make picks frontend call

--- frontend-ts/api/picks.ts
+++ frontend-ts/api/picks.ts
@@ -1,1 +1,28 @@
-export async function makePicks();
+import { Pick } from '../src/types';
+export type CreatePicksRequest = {
+  picks: Pick[];
+};
+export type CreatePicksResponse = {
+  success: boolean;
+  message?: string;
+};
+
+export async function makePicks(
+  data: CreatePicksRequest,
+  token: string
+): Promise<CreatePicksResponse> {
+  console.log('Pick data:', data);
+  const response = await fetch('http://localhost:500/picks/batch_make', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(data),
+  });
+  if (!response.ok) {
+    console.log(response);
+    throw new Error('Failed to create group');
+  }
+  return response.json();
+}
