dc45e0231af734b02adb01acf8d29160fdfc8143
Brendan Moorehead <brendan.moorehead11@gmail.com>
2025-02-26 20:05:26 -0500

    backend for confirming picks

--- backend/cron-jobs/confirmPicks.js
+++ backend/cron-jobs/confirmPicks.js
@@ -1,12 +1,46 @@
 import { queryUnresolvedPicks, queryScores } from './database.js';
+import supabase from '../db.js';
 export async function confirmPicks() {
   const unresolvedPicks = await queryUnresolvedPicks();
   const scores = await queryScores();
+  console.log(scores);
 
-  unresolvedPicks.map((pick) => {
-    const game = scores.find((score) => score.game_id === pick.game_id);
-    if ()
-  });
+  const updatedPicks = unresolvedPicks
+    .map((pick) => {
+      const game = scores.find((score) => score.game_id === pick.game_id);
+      if (!game) return;
+      if (!game.winner && (game.status === 'Final' || game.status === 'F/OT')) {
+        return {
+          ...pick,
+          result: false,
+          status: 'confirmed',
+        };
+      }
+      return {
+        ...pick,
+        result: pick.pick === game.winner,
+        status: 'confirmed',
+      };
+    })
+    .filter(Boolean);
+  console.log(updatedPicks);
+  await batchConfirmedPicks(updatedPicks);
+}
+
+export async function batchConfirmedPicks(picks) {
+  console.log('Batching confirmed picks to DB...');
+  try {
+    const { data, error } = await supabase
+      .from('user_picks')
+      .upsert(picks, { onConflict: ['game_id', 'group_id', 'made_by'] })
+      .select();
+
+    console.log(data);
+    if (error) throw error;
+    console.log('Batched confirmed picks successfully');
+  } catch (error) {
+    console.log('Failed to batch confirm picks: ', error);
+  }
 }
 
 confirmPicks();
